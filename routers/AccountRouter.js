6d1913e5ef2406ff20e2500af819a4fb62a3b660
jedelman <edelmanja@gmail.com>
2017-10-09 00:16:05 -0400

    initial commit

--- routers/AccountRouter.js
+++ routers/AccountRouter.js
@@ -2,6 +2,16 @@ const express = require("express");
 exports.create = repo =>{
   var router = express.Router();
   router.get("/", getCurrentAccount)
+  router.post("/login", login)
+  function login(req, res){
+    var success = repo.attemptLogin(req.body.userName, req.body.password);
+    res.send({
+      loginAttempt:{
+        success:success,
+        account:repo.getCurrentAccount()
+      }
+    });
+  }
   function getCurrentAccount(req, res){
     res.send(repo.getCurrentAccount())
   }
